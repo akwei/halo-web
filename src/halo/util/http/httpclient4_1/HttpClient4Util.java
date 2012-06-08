0963a0138a2b8426e1e317280270b798eb9fe3b1
akwei <ak478288@gmail.com>
2012-06-08 22:19:47 +0800

    update

--- src/halo/util/http/httpclient4_1/HttpClient4Util.java
+++ src/halo/util/http/httpclient4_1/HttpClient4Util.java
@@ -15,6 +15,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
@@ -50,42 +51,41 @@ public class HttpClient4Util {
         return httpclient;
     }
 
-    public HttpResp execute(HttpRequestBase request)
-            throws ClientProtocolException, IOException {
-        HttpEntity entity = null;
-        try {
-            HttpResponse httpResponse = httpclient.execute(request);
-            HttpResp httpResp = new HttpResp();
-            httpResp.setStatusCode(httpResponse.getStatusLine().getStatusCode());
-            entity = httpResponse.getEntity();
-            httpResp.setBytes(EntityUtils.toByteArray(entity));
-            return httpResp;
-        }
-        catch (ClientProtocolException e) {
-            throw e;
-        }
-        catch (IOException e) {
-            throw e;
-        }
-        finally {
-            if (entity != null) {
-                EntityUtils.consume(entity);
-            }
-        }
-    }
-
+    /**
+     * 简化的get
+     * 
+     * @param url
+     * @return
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
     public HttpResp doGet(String url) throws ClientProtocolException,
             IOException {
         return this.doGet(url, null, null);
     }
 
+    /**
+     * 详细get
+     * 
+     * @param url
+     * @param httpParameter
+     *            参考{@link HttpParameter}
+     * @param charset
+     *            参数编码
+     * @return 参考{@link HttpResp}
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
     public HttpResp doGet(String url, HttpParameter httpParameter,
             String charset) throws ClientProtocolException, IOException {
         StringBuilder sb = new StringBuilder(url);
-        if (url.indexOf("?") == -1) {
-            sb.append("?");
-        }
-        if (httpParameter != null) {
+        if (httpParameter != null && !httpParameter.isAllParameterEmpty()) {
+            if (url.indexOf("?") == -1) {
+                sb.append("?");
+            }
+            if (!url.endsWith("?")) {
+                sb.append("&");
+            }
             for (BasicParameter o : httpParameter.getBasicParameters()) {
                 sb.append(URLEncoder.encode(o.getName(), charset));
                 sb.append("=");
@@ -100,14 +100,60 @@ public class HttpClient4Util {
         return this.execute(httpGet);
     }
 
-    public HttpResp doPostBody(String url, String value, String charset)
+    /**
+     * post 字符数据
+     * 
+     * @param url
+     * @param value
+     *            post的字符串数据
+     * @param charset
+     *            参数编码
+     * @return
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
+    public HttpResp doPostStringBody(String url, String string, String charset)
             throws ClientProtocolException, IOException {
-        HttpEntity entity = new StringEntity(value, charset);
+        HttpEntity entity = new StringEntity(string, charset);
+        return this.doPostBody(url, entity);
+    }
+
+    /**
+     * post 字节数据
+     * 
+     * @param url
+     * @param bytes
+     *            字节数据
+     * @return
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
+    public HttpResp doPostBytesBody(String url, byte[] bytes)
+            throws ClientProtocolException, IOException {
+        HttpEntity entity = new ByteArrayEntity(bytes);
+        return this.doPostBody(url, entity);
+    }
+
+    private HttpResp doPostBody(String url, HttpEntity entity)
+            throws ClientProtocolException, IOException {
         HttpPost httpPost = new HttpPost(url);
         httpPost.setEntity(entity);
         return this.execute(httpPost);
     }
 
+    /**
+     * post
+     * 
+     * @param url
+     * @param httpParameter
+     *            参数信息
+     *            参考{@link HttpParameter}
+     * @param charset
+     *            参数编码
+     * @return 参考{@link HttpResp}
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
     public HttpResp doPost(String url, HttpParameter httpParameter,
             String charset) throws ClientProtocolException, IOException {
         HttpPost httpPost = new HttpPost(url);
@@ -133,21 +179,42 @@ public class HttpClient4Util {
         return this.execute(httpPost);
     }
 
-    public void close(HttpResponse httpResponse) throws IOException {
-        HttpEntity entity = httpResponse.getEntity();
-        if (entity != null) {
-            EntityUtils.consume(entity);
+    /**
+     * 具体执行过程
+     * 
+     * @param request
+     * @return
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
+    private HttpResp execute(HttpRequestBase request)
+            throws ClientProtocolException, IOException {
+        HttpEntity entity = null;
+        try {
+            HttpResponse httpResponse = httpclient.execute(request);
+            HttpResp httpResp = new HttpResp();
+            httpResp.setStatusCode(httpResponse.getStatusLine().getStatusCode());
+            entity = httpResponse.getEntity();
+            httpResp.setBytes(EntityUtils.toByteArray(entity));
+            return httpResp;
+        }
+        catch (ClientProtocolException e) {
+            throw e;
+        }
+        catch (IOException e) {
+            throw e;
+        }
+        finally {
+            if (entity != null) {
+                EntityUtils.consume(entity);
+            }
         }
     }
 
+    /**
+     * 最终可释放资源调用
+     */
     public void shutdown() {
         this.httpclient.getConnectionManager().shutdown();
-    }
-
-    public static void main(String[] args) throws ClientProtocolException,
-            IOException {
-        HttpClient4Util util = new HttpClient4Util(10000);
-        HttpResp httpResp = util.doGet("http://www.yibao.com");
-        System.out.println(httpResp.getText("utf-8"));
     }
 }
